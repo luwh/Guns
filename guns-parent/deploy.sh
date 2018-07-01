e40950d03c5b1e0503d1911205818aa8472947bb
luwh <920539690@qq.com>
2018-07-01 23:02:53 +0800

    deploy.sh

---
+++ guns-parent/deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#编译+部署order站点
+
+#需要配置如下参数
+# 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+# 输入你的环境上tomcat的全路径
+# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+
+### base 函数
+killTomcat()
+{
+    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    echo "tomcat Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+      echo "no tomcat pid alive"
+    else
+      kill -9 $pid
+    fi
+}
+
+cd /root/.jenkins/workspace/DeployGuns/guns/guns-parent
+mvn clean package -Dmaven.test.skip=true
+
+# 停tomcat
+killTomcat
+
+# 启动
+cd /root/.jenkins/workspace/DeployGuns/guns/guns-admin/target
+java -jar guns-admin-1.0.0-SNAPSHOT.jar
